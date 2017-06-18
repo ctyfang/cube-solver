dd3ae0f49b6cfee3e9ba342bccc5c8ab05ee4148
ctyfang <ctyfang@gmail.com>
2017-06-18 15:16:28 -0700

    Develop function for consolidating functions for extracting cube state, cube re-orientation, and cube solving. Main.py is to be executed when machine is turned on and cube is in place

---
+++ code/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 18 14:18:13 2017
+
+@author: Carter
+"""
+import numpy as p
+import cuberdr as cubecam
+import cubesolver as decode
+import motormanipulator as motor
+
+# capture cube state
+state = cubecam.read()
+
+# check cube state for inconsistencies
+
+# plug cube state into solver function
+decode.gen_moveset(state)
+moves = decode.moves
+
+# execute solve and re-orientation scripts
+motor.execute_solve(moves)
